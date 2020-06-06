0f00d51b21343571b8823982bf80acf74457e8ac
liuxin <liuxin@imo.im>
2020-06-06 19:45:24 +0800

    notes

--- apue.h
+++ apue.h
@@ -1,133 +1,133 @@
 /*
-* Our own header, to be included before all standard system headers.
-*/
-#ifndef _APUE_H
-#define _APUE_H
+ * Our own header, to be included before all standard system headers.
+ */
+#ifndef	_APUE_H
+#define	_APUE_H
 
 #define _POSIX_C_SOURCE 200809L
 
-#if defined(SOLARIS)        /* Solaris 10 */
+#if defined(SOLARIS)		/* Solaris 10 */
 #define _XOPEN_SOURCE 600
 #else
 #define _XOPEN_SOURCE 700
 #endif
 
-#include <sys/types.h>      /* some systems still require this */
+#include <sys/types.h>		/* some systems still require this */
 #include <sys/stat.h>
-#include <sys/termios.h>    /* for winsize */
+#include <sys/termios.h>	/* for winsize */
 #if defined(MACOS) || !defined(TIOCGWINSZ)
 #include <sys/ioctl.h>
 #endif
 
-#include <stdio.h>      /* for convenience */
-#include <stdlib.h>     /* for convenience */
-#include <stddef.h>     /* for offsetof */
-#include <string.h>     /* for convenience */
-#include <unistd.h>     /* for convenience */
-#include <signal.h>     /* for SIG_ERR */
-
-#define MAXLINE 4096    /* max line length */
-
-/*
-* Default file access permissions for new files.
-*/
-#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#include <stdio.h>		/* for convenience */
+#include <stdlib.h>		/* for convenience */
+#include <stddef.h>		/* for offsetof */
+#include <string.h>		/* for convenience */
+#include <unistd.h>		/* for convenience */
+#include <signal.h>		/* for SIG_ERR */
+#define	MAXLINE	4096			/* max line length */
 
 /*
-* Default permissions for new directories.
-*/
-#define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
-
-typedef void Sigfunc(int); /* for signal handlers */
-
-#define min(a,b) ((a) < (b) ? (a) : (b))
-#define max(a,b) ((a) > (b) ? (a) : (b))
+ * Default file access permissions for new files.
+ */
+#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
 /*
-* Prototypes for our own functions.
-*/
-char *path_alloc(size_t *);                /* Figure 2.16 */
-long open_max(void);                       /* Figure 2.17 */
+ * Default permissions for new directories.
+ */
+#define	DIR_MODE	(FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 
-int set_cloexec(int);                      /* Figure 13.9 */
-void clr_fl(int, int);
-void set_fl(int, int);                     /* Figure 3.12 */
+typedef	void	Sigfunc(int);	/* for signal handlers */
 
-void pr_exit(int);                         /* Figure 8.5 */
+#define	min(a,b)	((a) < (b) ? (a) : (b))
+#define	max(a,b)	((a) > (b) ? (a) : (b))
 
-void pr_mask(const char *);                /* Figure 10.14 */
-Sigfunc *signal_intr(int, Sigfunc *);      /* Figure 10.19 */
+/*
+ * Prototypes for our own functions.
+ */
+char	*path_alloc(size_t *);				/* {Prog pathalloc} */
+long	 open_max(void);					/* {Prog openmax} */
 
-void daemonize(const char *);              /* Figure 13.1 */
+int		 set_cloexec(int);					/* {Prog setfd} */
+void	 clr_fl(int, int);
+void	 set_fl(int, int);					/* {Prog setfl} */
 
-void sleep_us(unsigned int);               /* Exercise 14.5 */
-ssize_t readn(int, void *, size_t);        /* Figure 14.24 */
-ssize_t writen(int, const void *, size_t); /* Figure 14.24 */
+void	 pr_exit(int);						/* {Prog prexit} */
 
-int fd_pipe(int *);                        /* Figure 17.2 */
-int recv_fd(int, ssize_t (*func)(int,
-            const void *, size_t));        /* Figure 17.14 */
-int send_fd(int, int);                     /* Figure 17.13 */
-int send_err(int, int,
-             const char *);                /* Figure 17.12 */
-int serv_listen(const char *);             /* Figure 17.8 */
-int serv_accept(int, uid_t *);             /* Figure 17.9 */
-int cli_conn(const char *);                /* Figure 17.10 */
-int buf_args(char *, int (*func)(int,
-             char **));                    /* Figure 17.23 */
+void	 pr_mask(const char *);				/* {Prog prmask} */
+Sigfunc	*signal_intr(int, Sigfunc *);		/* {Prog signal_intr_function} */
 
-int tty_cbreak(int);                       /* Figure 18.20 */
-int tty_raw(int);                          /* Figure 18.20 */
-int tty_reset(int);                        /* Figure 18.20 */
-void tty_atexit(void);                     /* Figure 18.20 */
-struct termios *tty_termios(void);         /* Figure 18.20 */
+void	 daemonize(const char *);			/* {Prog daemoninit} */
 
-int ptym_open(char *, int);                /* Figure 19.9 */
-int ptys_open(char *);                     /* Figure 19.9 */
-#ifdef TIOCGWINSZ
-pid_t pty_fork(int *, char *, int, const struct termios *,
-               const struct winsize *);    /* Figure 19.10 */
+void	 sleep_us(unsigned int);			/* {Ex sleepus} */
+ssize_t	 readn(int, void *, size_t);		/* {Prog readn_writen} */
+ssize_t	 writen(int, const void *, size_t);	/* {Prog readn_writen} */
+
+int		 fd_pipe(int *);					/* {Prog sock_fdpipe} */
+int		 recv_fd(int, ssize_t (*func)(int,
+		         const void *, size_t));	/* {Prog recvfd_sockets} */
+int		 send_fd(int, int);					/* {Prog sendfd_sockets} */
+int		 send_err(int, int,
+		          const char *);			/* {Prog senderr} */
+int		 serv_listen(const char *);			/* {Prog servlisten_sockets} */
+int		 serv_accept(int, uid_t *);			/* {Prog servaccept_sockets} */
+int		 cli_conn(const char *);			/* {Prog cliconn_sockets} */
+int		 buf_args(char *, int (*func)(int,
+		          char **));				/* {Prog bufargs} */
+
+int		 tty_cbreak(int);					/* {Prog raw} */
+int		 tty_raw(int);						/* {Prog raw} */
+int		 tty_reset(int);					/* {Prog raw} */
+void	 tty_atexit(void);					/* {Prog raw} */
+struct termios	*tty_termios(void);			/* {Prog raw} */
+
+int		 ptym_open(char *, int);			/* {Prog ptyopen} */
+int		 ptys_open(char *);					/* {Prog ptyopen} */
+#ifdef	TIOCGWINSZ
+pid_t	 pty_fork(int *, char *, int, const struct termios *,
+		          const struct winsize *);	/* {Prog ptyfork} */
 #endif
 
-int lock_reg(int, int, int, off_t, int, off_t); /* Figure 14.5 */
+int		lock_reg(int, int, int, off_t, int, off_t); /* {Prog lockreg} */
 
-#define read_lock(fd, offset, whence, len) \
-            lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
-#define readw_lock(fd, offset, whence, len) \
-            lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
-#define write_lock(fd, offset, whence, len) \
-            lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
-#define writew_lock(fd, offset, whence, len) \
-            lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
-#define un_lock(fd, offset, whence, len) \
-            lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
+#define	read_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define	readw_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+#define	write_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define	writew_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define	un_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
 
-pid_t lock_test(int, int, off_t, int, off_t);   /* Figure 14.6 */
+pid_t	lock_test(int, int, off_t, int, off_t);		/* {Prog locktest} */
 
-#define is_read_lockable(fd, offset, whence, len) \
-            (lock_test((fd), F_RDLCK, (offset), (whence), (len)) == 0)
-#define is_write_lockable(fd, offset, whence, len) \
-            (lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
+#define	is_read_lockable(fd, offset, whence, len) \
+			(lock_test((fd), F_RDLCK, (offset), (whence), (len)) == 0)
+#define	is_write_lockable(fd, offset, whence, len) \
+			(lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
 
-void err_msg(const char *, ...);            /* Appendix B */
-void err_dump(const char *, ...) __attribute__((noreturn));
-void err_quit(const char *, ...) __attribute__((noreturn));
-void err_cont(int, const char *, ...);
-void err_exit(int, const char *, ...) __attribute__((noreturn));
-void err_ret(const char *, ...);
-void err_sys(const char *, ...) __attribute__((noreturn));
+void	err_msg(const char *, ...);			/* {App misc_source} */
+void	err_dump(const char *, ...) __attribute__((noreturn));
+void	err_quit(const char *, ...) __attribute__((noreturn));
+void	err_cont(int, const char *, ...);
+void	err_exit(int, const char *, ...) __attribute__((noreturn));
+void	err_ret(const char *, ...);
+void	err_sys(const char *, ...) __attribute__((noreturn));
 
-void log_msg(const char *, ...);            /* Appendix B */
-void log_open(const char *, int, int);
-void log_quit(const char *, ...) __attribute__((noreturn));
-void log_ret(const char *, ...);
-void log_sys(const char *, ...) __attribute__((noreturn));
-void log_exit(int, const char *, ...) __attribute__((noreturn));
+void	log_msg(const char *, ...);			/* {App misc_source} */
+void	log_open(const char *, int, int);
+void	log_quit(const char *, ...) __attribute__((noreturn));
+void	log_ret(const char *, ...);
+void	log_sys(const char *, ...) __attribute__((noreturn));
+void	log_exit(int, const char *, ...) __attribute__((noreturn));
 
-void TELL_WAIT(void);        /* parent/child from Section 8.9 */
-void TELL_PARENT(pid_t);
-void TELL_CHILD(pid_t);
-void WAIT_PARENT(void);
-void WAIT_CHILD(void);
+void	TELL_WAIT(void);		/* parent/child from {Sec race_conditions} */
+void	TELL_PARENT(pid_t);
+void	TELL_CHILD(pid_t);
+void	WAIT_PARENT(void);
+void	WAIT_CHILD(void);
 
-#endif /* _APUE_H */
+#include "error.c"
+#endif	/* _APUE_H */
---
+++ cp.cpp
@@ -0,0 +1,17 @@
+#include"apue.h"
+#define BUFFERSIZE 4096
+int main(void){
+    int n;
+    char buf[BUFFERSIZE];
+    while ((n = read(STDIN_FILENO,buf,BUFFERSIZE)) > 0)
+    {
+        if(write(STDOUT_FILENO,buf,BUFFERSIZE) != n)
+        {
+            err_sys("write error");
+        }
+    }
+    if (n < 0)
+        err_sys("read error");
+    
+    exit(0);
+}
