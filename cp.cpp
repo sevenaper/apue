c8235e5d589bab6ccd8df5dad369997f517712f2
aper <lxsdwlhh@163.com>
2020-06-20 16:07:45 +0800

    format

--- cp.cpp
+++ cp.cpp
@@ -1,17 +1,18 @@
-#include"apue.h"
+#include "apue.h"
 #define BUFFERSIZE 4096
-int main(void){
+int main(void)
+{
     int n;
     char buf[BUFFERSIZE];
-    while ((n = read(STDIN_FILENO,buf,BUFFERSIZE)) > 0)
+    while ((n = read(STDIN_FILENO, buf, BUFFERSIZE)) > 0)
     {
-        if(write(STDOUT_FILENO,buf,BUFFERSIZE) != n)
+        if (write(STDOUT_FILENO, buf, BUFFERSIZE) != n)
         {
             err_sys("write error");
         }
     }
     if (n < 0)
         err_sys("read error");
-    
+
     exit(0);
 }
--- ls.cpp
+++ ls.cpp
@@ -1,17 +1,20 @@
-#include"apue.h"
-#include<dirent.h>
-
-int main(int argc,char* argv[]){
+#include "apue.h"
+#include <dirent.h>
+int main(int argc, char *argv[])
+{
     DIR *dp;
     dirent *dirp;
-    if(argc != 2){
+    if (argc != 2)
+    {
         err_quit("usage: ls directory_name");
     }
-    if((dp = opendir(argv[1])) == NULL){
-        err_sys("cannot open %s",&argv[1]);
+    if ((dp = opendir(argv[1])) == NULL)
+    {
+        err_sys("cannot open %s", &argv[1]);
     }
-    while((dirp = readdir(dp)) != NULL){
-        printf("%s\n",dirp->d_name);
+    while ((dirp = readdir(dp)) != NULL)
+    {
+        printf("%s\n", dirp->d_name);
     }
     closedir(dp);
     exit(0);
